d642ae8467042037b19873eb4059c0c82f5d5123
teo <tkatsigiannis@gmail.com>
2025-06-20 18:25:10 +0300

    feat: refactor preprocessing with train-test split, update encoders, and improve scaling logic

--- code/main.py
+++ code/main.py
@@ -1,11 +1,10 @@
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from catboost import CatBoostClassifier, Pool
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.model_selection import train_test_split
 
-##############################################
-########### Step 1: Preprocessing ############
-##############################################
+#################################################
+# Step 1: Load Dataset - Split - Preprocessing ##
+#################################################
 
 # Load dataset from disk
 CREDIT_SCORE_DATASET_PATH = "../dataset/credit_score.csv"
@@ -15,6 +14,10 @@ print("Column names:", df.columns.tolist())
 
 X = df.drop(columns=['DEFAULT', 'CUST_ID'])
 y = df['DEFAULT']
+
+
+# Split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Display information about missing values in X
 print("\nMissing values in X:")
@@ -26,39 +29,28 @@ print("\nMissing values in y:")
 print(y.isnull().sum())
 
 # Convert categorical columns to numerical
-categorical_columns = X.select_dtypes(include=['object']).columns
-for column in categorical_columns:
-    le = LabelEncoder()
-    X[column] = le.fit_transform(X[column])
+# Warning: For better stability, not modify a view but a full copy
+X_train = X_train.copy()
+X_test = X_test.copy()
 
-# Scale the X data
-X_scaled = StandardScaler().fit_transform(X)
-print("\nScaled X:")
-print(X_scaled)
+# Warning: we are labeling using X_train and not X
+categorical_columns = X_train.select_dtypes(include=['object']).columns
+encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
+X_train[categorical_columns] = encoder.fit_transform(X_train[categorical_columns])
+X_test[categorical_columns] = encoder.transform(X_test[categorical_columns])
 
-##########################################################
-########### Step 2: Feature Selection - CatBoost #########
-##########################################################
-model = CatBoostClassifier(
-    iterations=100,
-    learning_rate=0.1,
-    depth=2,
-    loss_function='Logloss',
-    verbose=False
-)
-model.fit(X_scaled, y)
-importance_features = model.get_feature_importance()
+# Scale the data
+# Warning: we are using fit_transform and transform for X_train and X_test correspondingly
+scaler = StandardScaler()
+X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
+X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns=X_test.columns)
 
-print("\nFeature importance:")
-print(importance_features)
+print("X_train_scaled type:", type(X_train_scaled))
+print("X_train_scaled shape:", X_train_scaled.shape)
 
-top_features = np.argsort(importance_features)[-5:]
-print("\nTop 5 features:")
-print(top_features)
-print(len(top_features))
+print("X_test_scaled type:", type(X_test_scaled))
+print("X_test_scaled shape:", X_test_scaled.shape)
 
-X_selected = X_scaled[:, top_features]
-print("\nSelected features:")
-print(X_selected)
-
-
+#############################################
+# Step 2: Feature selection using Spearman ##
+#############################################
