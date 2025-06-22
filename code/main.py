fc6410e72e3f77328af70a8bc97ca4996616e109
teo <tkatsigiannis@gmail.com>
2025-06-22 11:51:26 +0300

    refactor: Modify categorical encoder to a function Modify data scaler into a function

--- code/main.py
+++ code/main.py
@@ -1,9 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, classification_report
 from sklearn.manifold import TSNE
 from catboost import CatBoostClassifier
 from scipy.stats import pearsonr
+
+##################################################
+# Step 0: We are creating some helpful functions #
+##################################################
+
+def modify_categorical_cols(x):
+    """
+    Converts categorical columns in a DataFrame to numerical values using OrdinalEncoder.
+
+    Args:
+        x (pd.DataFrame): Input DataFrame containing categorical columns
+
+    Returns:
+        pd.DataFrame: DataFrame with categorical columns converted to numerical values
+    """
+
+    categorical_columns = x.select_dtypes(include=['object']).columns
+    encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
+    x[categorical_columns] = encoder.fit_transform(x[categorical_columns])
+    return x
+
+def scale_dataset(x):
+    """
+    Scales numerical features in a DataFrame using StandardScaler.
+
+    Args:
+        x (pd.DataFrame): Input DataFrame containing numerical features to be scaled
+
+    Returns:
+        pd.DataFrame: DataFrame with scaled numerical features, maintaining original column names
+    """
+
+    # NOTE: we are using fit_transform and transform for X_train and X_test correspondingly
+    scaler = StandardScaler()
+    return pd.DataFrame(scaler.fit_transform(x), columns=x.columns)
 
 #################################################
 # Step 1: Load Dataset - Split - Preprocessing ##
@@ -28,19 +65,15 @@ print("\nMissing values in y:")
 print(y.isnull().sum())
 
 # Convert categorical columns to numerical
-# Warning: For better stability, not modify a view but a full copy
+
+# NOTE: For better stability, not modify a view but a full copy
 X = X.copy()
 
-# Warning: we are labeling using X_train and not X
-categorical_columns = X.select_dtypes(include=['object']).columns
-encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
-X[categorical_columns] = encoder.fit_transform(X[categorical_columns])
+# Categorical to numerical labels to X
+X = modify_categorical_cols(X)
 
-# Scale the data
-# Warning: we are using fit_transform and transform for X_train and X_test correspondingly
-scaler = StandardScaler()
-X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
-
+# Scale dataset
+X_scaled = scale_dataset(X)
 print("X_train_scaled shape:", X_scaled.shape)
 
 ###########################
@@ -78,7 +111,6 @@ print(importance_df)
 #######################################################
 
 # Correlation between features and target
-
 correlation_scores = []
 p_values = []
 
@@ -119,3 +151,51 @@ plt.grid(True)
 plt.colorbar(scatter, label = 'Default (0=No, 1=Yes)')
 plt.show()
 
+#####################################################
+# Step 5: Feature Selection from combined Criteria ##
+#####################################################
+# XXX: Use the scatter graph to deside the number of the features.
+top_n = 5
+
+# Take importance col with "Feature" indices
+importance = importance_df.set_index("Feature")["Importance"]
+# Take pearson correlation col with "Feature" indices
+correlation = correlation_df.set_index("Feature")["Pearson"]
+
+ranked_importance = importance.rank(ascending=False)
+ranked_correlation = correlation.rank(ascending=False)
+print("Ranked correlation:")
+print(ranked_correlation)
+print("Ranked ranked_importance:")
+print(ranked_importance)
+
+combined_rank = (ranked_correlation+ranked_importance)/2
+print("Combined rank:")
+print(combined_rank)
+
+combined_rank_df = combined_rank.sort_values().reset_index()
+combined_rank_df.columns = ["Feature", "AverageRank"]
+print("Top 5 features:")
+print(combined_rank_df.head(top_n))
+
+# n-top feature
+selected_features = combined_rank_df.head(top_n)["Feature"].tolist()
+print("Selected features:", selected_features)
+
+# NOTE: We what to rescale and retrain the catboost algorithm
+# using the ->->initial<-<- dataset
+X_selected = df[selected_features]
+y_final = df['DEFAULT']
+
+
+# Split data to train and test sets
+
+# modify categorical cols to numerical
+
+# scale data
+
+
+############################################
+# Step 6: Create a new catboost classifier #
+#     and retrain with df_selected Dataset #
+############################################
